3fbbdba4f1ff8b8797b70ef7e23090f6c46270bd
csPython <cspython@AD25TF04NGF1J-114.local>
2019-03-13 12:47:45 -0400

    added one square in window

--- checkersPH.py
+++ checkersPH.py
@@ -1,1 +1,16 @@
 ##checkers.py by Pauline Harrell
+
+from graphics import *
+
+sqSz = 50
+
+chWin = GraphWin(" Checkers" , sqSz * 10, sqSz * 10)
+chWin.setCoords(0, 0, sqSz * 10, sqSz * 10)
+
+
+sQ = Rectangle(Point(sqSz, sqSz), Point(sqSz + sqSz, sqSz + sqSz))
+sQ.setFill(color_rgb(255,0,0))
+sQ.draw(chWin)
+                                   
+chWin.getMouse()
+chWin.close()                        
